2720c5e22664b4de9da412ebedde5966edafc207
Jyen <jerry160726@gmail.com>
2022-03-14 21:48:53 +0800

    productModal

---
+++ src/components/ProductModal.vue
@@ -0,0 +1,175 @@
+<template>
+  <div
+    class="modal fade"
+    id="productModal"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+    ref="modal"
+  >
+    <div class="modal-dialog modal-xl" role="document">
+      <div class="modal-content border-0">
+        <div class="modal-header bg-dark text-white">
+          <h5 class="modal-title" id="exampleModalLabel">
+            <span>{{ product.title }}</span>
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-sm-6">
+              <img class="img-fluid" :src="product.imageUrl" alt="" />
+            </div>
+            <div class="col-sm-6">
+              <span class="badge bg-primary rounded-pill">{{
+                product.category
+              }}</span>
+              <p>商品描述：{{ product.content }}</p>
+              <p>商品內容：{{ product.description }}</p>
+              <div class="h5" v-if="product.price === product.origin_price">
+                {{ product.price }} 元
+              </div>
+              <div v-else>
+                <del class="h6">原價 {{ product.origin_price }} 元</del>
+                <div class="h5">現在只要 {{ product.price }} 元</div>
+              </div>
+              <div>
+                <div class="input-group">
+                  <input
+                    type="number"
+                    class="form-control"
+                    min="1"
+                    v-model.number="qty"
+                  />
+                  <button
+                    type="button"
+                    class="btn btn-primary"
+                    @click="addToCart"
+                  >
+                    加入購物車
+                  </button>
+                </div>
+              </div>
+            </div>
+            <!-- col-sm-6 end -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div
+    class="modal fade"
+    id="productModal"
+    tabindex="-1"
+    role="dialog"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+    ref="modal"
+  >
+    <div class="modal-dialog modal-xl" role="document">
+      <div class="modal-content border-0">
+        <div class="modal-header bg-dark text-white">
+          <h5 class="modal-title" id="exampleModalLabel">
+            <span>{{ product.title }}</span>
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-sm-6">
+              <img class="img-fluid" :src="product.imageUrl" alt="" />
+            </div>
+            <div class="col-sm-6">
+              <span class="badge bg-primary rounded-pill">{{
+                product.category
+              }}</span>
+              <p>商品描述：{{ product.content }}</p>
+              <p>商品內容：{{ product.description }}</p>
+              <div class="h5" v-if="product.price === product.origin_price">
+                {{ product.price }} 元
+              </div>
+              <div v-else>
+                <del class="h6">原價 {{ product.origin_price }} 元</del>
+                <div class="h5">現在只要 {{ product.price }} 元</div>
+              </div>
+              <div>
+                <div class="input-group">
+                  <input
+                    type="number"
+                    class="form-control"
+                    min="1"
+                    v-model.number="qty"
+                  />
+                  <button
+                    type="button"
+                    class="btn btn-primary"
+                    @click="addToCart"
+                  >
+                    加入購物車
+                  </button>
+                </div>
+              </div>
+            </div>
+            <!-- col-sm-6 end -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Modal from 'bootstrap/js/dist/modal'
+export default {
+  props: ['id'],
+  template: '#userProductModal',
+  data () {
+    return {
+      modal: {},
+      product: {},
+      qty: 1
+    }
+  },
+  watch: {
+    id () {
+      this.getProduct()
+    }
+  },
+  methods: {
+    openModal () {
+      this.modal.show()
+    },
+    closeModal () {
+      this.modal.hide()
+    },
+    getProduct () {
+      this.$http
+        .get(
+          `${process.env.VUE_APP_API}/api/${process.env.VUE_APP_PATH}/product/${this.id}`
+        )
+        .then((res) => {
+          console.log(res)
+          this.product = res.data.product
+        })
+    },
+    addToCart () {
+      this.$emit('add-cart', this.product.id, this.qty)
+    }
+  },
+  mounted () {
+    this.modal = new Modal(this.$refs.modal)
+  }
+}
+</script>
